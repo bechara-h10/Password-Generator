9c7baa1d673510a4521c0547cd9242a4659ca8cf
bechara-h10 <bechara.hosri1@gmail.com>
2023-04-29 23:26:50 +0300

    Final Version

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ const passwordInput = document.querySelector('.input-container input')
 const settings = document.querySelectorAll('.setting input')
 const copySpan = document.querySelector('.input-container span')
 const passwordStrength = document.querySelector('.password-strength')
-
+const generateButton = document.querySelector('.generate-button')
+const errorText = document.querySelector('.error-text')
 const characters = {
   lowercase: 'abcdefghijklmnopqrstuvwxyz',
   uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
   numbers: '0123456789',
   symbols: '!$%&|[](){}:;.,*+-#@<>~',
 }
+
+generatePassword()
+updateStrengthSlider()
 
 function generatePassword() {
   let staticPassword = ''
@@ -38,10 +42,54 @@ function generatePassword() {
     if (excludeDuplicate) {
       if (!randomPassword.includes(randomChar) && randomChar !== ' ') {
         randomPassword += randomChar
-      } else {
-        randomPassword += randomChar
       }
+    } else {
+      randomPassword += randomChar
     }
   }
+  lengthValue.textContent = randomPassword.length
   passwordInput.value = randomPassword
 }
+
+function updateStrengthSlider() {
+  passwordStrength.id =
+    lengthSlider.value >= 8 && lengthSlider.value <= 12
+      ? 'weak'
+      : lengthSlider.value > 12 && lengthSlider.value < 24
+      ? 'medium'
+      : 'strong'
+}
+
+generateButton.addEventListener('click', () => {
+  generatePassword()
+  updateStrengthSlider()
+})
+
+lengthSlider.addEventListener('input', () => {
+  generatePassword()
+  updateStrengthSlider()
+})
+
+settings.forEach((setting) => {
+  setting.addEventListener('change', () => {
+    generatePassword()
+    updateStrengthSlider()
+  })
+})
+
+copySpan.addEventListener('click', () => {
+  if (passwordInput.value == '') {
+    errorText.classList.add('active')
+    setTimeout(() => {
+      errorText.classList.remove('active')
+    }, 3000)
+    return
+  }
+  navigator.clipboard.writeText(passwordInput.value)
+  copySpan.textContent = 'done'
+  copySpan.style.color = '#43a047'
+  setTimeout(() => {
+    copySpan.textContent = 'copy_all'
+    copySpan.style.color = 'black'
+  }, 2000)
+})
